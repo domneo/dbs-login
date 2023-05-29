40ddd470a96cb2fd2512d6a3c0900f417937f972
Dominic Neo <dom.neo.ws@gmail.com>
2023-05-29 23:35:30 +0800

    button component

--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.scss']
+  styleUrls: ['./button.component.scss'],
 })
 export class ButtonComponent {
+  @Input() variant: 'primary' | 'secondary' = 'primary';
+  @Input()
+  set disabled(value: boolean | string) {
+    this.disabledBoolean =
+      value === 'true' || value.toString() === 'true' || value === '';
+  }
+  get disabled(): string {
+    return this.disabledBoolean.toString();
+  }
+  @Output() onClick = new EventEmitter();
 
+  disabledBoolean: boolean = false;
+
+  btnClick = () => {
+    this.onClick.emit();
+  };
 }
--- src/app/elements/elements.component.ts
+++ src/app/elements/elements.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-elements',
   templateUrl: './elements.component.html',
-  styleUrls: ['./elements.component.scss']
+  styleUrls: ['./elements.component.scss'],
 })
 export class ElementsComponent {
-
+  onButtonClick = () => {
+    console.log('clicked');
+  };
 }
