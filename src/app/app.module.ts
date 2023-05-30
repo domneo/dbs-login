00c098631af3d38d3bc261c49b0c3164bbcc127a
Dominic Neo <dom.neo.ws@gmail.com>
2023-05-31 00:44:37 +0800

    icon component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { ElementsComponent } from './elements/elements.component';
 import { ButtonComponent } from './button/button.component';
 import { InputFieldComponent } from './input-field/input-field.component';
+import { IconComponent } from './icon/icon.component';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { InputFieldComponent } from './input-field/input-field.component';
     ElementsComponent,
     ButtonComponent,
     InputFieldComponent,
+    IconComponent,
   ],
   imports: [BrowserModule],
   providers: [],
