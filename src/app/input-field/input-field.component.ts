092af27c9029bd28980ff0de2914c9939d630b27
Dominic Neo <dom.neo.ws@gmail.com>
2023-05-30 23:46:35 +0800

    input validation

--- src/app/input-field/input-field.component.ts
+++ src/app/input-field/input-field.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { v4 as uuidv4 } from 'uuid';
 
 @Component({
   selector: 'app-input-field',
@@ -6,6 +7,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrls: ['./input-field.component.scss'],
 })
 export class InputFieldComponent {
+  @Input() label: string = '';
   @Input() value: string = '';
   @Input() placeholder: string = '';
   @Input() type: string = 'text';
@@ -13,8 +15,10 @@ export class InputFieldComponent {
   @Input() icon: string = '';
   @Input() validationText: string = '';
   @Input() validationIcon: string = '';
-  @Input() validationType: 'success' | 'error' = 'success';
+  @Input() validationType: 'default' | 'success' | 'error' = 'default';
   @Output() onChange = new EventEmitter();
+
+  id = uuidv4();
 
   valueChange = (event: Event) => {
     this.onChange.emit((<HTMLInputElement>event.target).value);
